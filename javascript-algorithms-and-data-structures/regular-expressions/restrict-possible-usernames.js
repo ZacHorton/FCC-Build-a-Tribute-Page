2dc12a9aa54dc88c7d1e879626e9502fe73ff0b0
Zac Horton <45723831+ZacHorton@users.noreply.github.com>
2023-11-08 15:02:11 -0500

    Update restrict-possible-usernames.js
    https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/restrict-possible-usernames

--- javascript-algorithms-and-data-structures/regular-expressions/restrict-possible-usernames.js
+++ javascript-algorithms-and-data-structures/regular-expressions/restrict-possible-usernames.js
@@ -1,3 +1,3 @@
 let username = "JackOfAllTrades";
-let userCheck = /^[a-z]{2,}$|^[a-z]+[0-9]{2}$|^[a-z]{2,}[0-9]?$/i;
+let userCheck = /^[a-z]{2,}[0-9]?$|^[a-z]+[0-9]{2}$/i;
 let result = userCheck.test(username);
