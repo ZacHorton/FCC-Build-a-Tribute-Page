869617c509af3766d0407b0636800e257f4b3b08
Zac Horton <45723831+ZacHorton@users.noreply.github.com>
2023-11-13 21:41:35 -0500

    Update return-a-sorted-array-without-changing-the-original-array.js
    https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/functional-programming/return-a-sorted-array-without-changing-the-original-array

--- javascript-algorithms-and-data-structures/functional-programming/return-a-sorted-array-without-changing-the-original-array.js
+++ javascript-algorithms-and-data-structures/functional-programming/return-a-sorted-array-without-changing-the-original-array.js
@@ -2,9 +2,7 @@ const globalArray = [5, 6, 3, 2, 9];
 
 function nonMutatingSort(arr) {
   let newArr = arr.slice(0, arr.length);
-  let sortedArr = newArr.sort(function(a, b) {
-    return a > b ? 1 : a < b ? -1 : 0;
-  });
+  let sortedArr = newArr.sort((a, b) => a > b ? 1 : a < b ? -1 : 0);
   return sortedArr;
   }
 
