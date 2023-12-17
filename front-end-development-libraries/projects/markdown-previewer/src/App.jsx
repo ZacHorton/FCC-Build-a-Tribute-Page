d0e3d616e80d0ac9bf1d9b4b66451e9ea86d355c
Zac Horton <fullstackzac@gmail.com>
2023-12-17 13:28:05 -0500

    Update to pass tests

--- front-end-development-libraries/projects/markdown-previewer/src/App.jsx
+++ front-end-development-libraries/projects/markdown-previewer/src/App.jsx
@@ -1,13 +1,59 @@
 import { useState } from "react";
+import { marked } from "marked";
+import DOMPurify from "dompurify";
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
-  // a heading element (H1 size), a sub heading element (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
+  const [text, setText] = useState(`
+  # H1 heading
+  ## H2 heading
+  ### H3 heading
+  **Bold text**
+
+  *Italicized text*
+  > This is a blockquote
+
+  Ordered list:
+  1. First item
+  2. Second item
+  3. Third item
+
+  Unordered list:
+  - First item
+  - Second item
+  - Third item
+
+  Inline code:
+  \`<div>This is a div tag<div>\`
+
+  Fenced code block:
+  \`\`\`
+  function sum(a, b) {
+    return a + b;
+  }
+  \`\`\`
+
+  Build a Markdown Previewer:
+  [Link](https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-markdown-previewer)
+  
+  Markdown symbol:
+  ![Markdown symbol](https://upload.wikimedia.org/wikipedia/commons/4/48/Markdown-mark.svg)
+  `);
   return (
     <>
-      <textarea id="editor"></textarea>
-      <div id="preview">{text}</div>
+      <textarea
+        id="editor"
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+        value={text}
+      ></textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: DOMPurify.sanitize(marked(text, { breaks: true })),
+        }}
+      ></div>
     </>
   );
 }
