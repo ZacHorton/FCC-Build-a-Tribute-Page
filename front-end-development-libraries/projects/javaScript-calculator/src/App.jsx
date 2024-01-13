c078d042658d1bdb9a12742bad6f4a0afdaf999c
Zac Horton <fullstackzac@gmail.com>
2024-01-13 15:42:19 -0500

    Downgraded to React 17 to stabilize test cases

--- front-end-development-libraries/projects/javaScript-calculator/src/App.jsx
+++ front-end-development-libraries/projects/javaScript-calculator/src/App.jsx
@@ -2,33 +2,77 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [total, setTotal] = useState(0);
+  const [display, setDisplay] = useState("");
+
+  const handleCLick = (event) => {
+    switch (event.target.innerText) {
+      case "AC":
+        setDisplay("");
+        break;
+      case "9":
+      case "8":
+      case "7":
+      case "6":
+      case "5":
+      case "4":
+      case "3":
+      case "2":
+      case "1":
+      case "/":
+      case "*":
+      case "-":
+      case "+":
+        setDisplay(display + event.target.innerText);
+        break;
+      case "=":
+        console.log(display);
+        setDisplay(eval(display));
+        break;
+      case "0":
+        if (display.charAt(0) != "") {
+          setDisplay(display + event.target.innerText);
+        }
+        break;
+      case ".":
+        // Prevents consecutive decimals
+        const regex = /\.\./;
+        if (!regex.test(display + ".")) {
+          setDisplay(display + event.target.innerText);
+        }
+        break;
+    }
+  };
 
   return (
     <>
       <div id="container">
         <div id="calculator">
-          <div id="display">0</div>
-          <button id="equals">=</button>
-          <button id="zero">0</button>
-          <button id="one">1</button>
-          <button id="two">2</button>
-          <button id="three">3</button>
-          <button id="four">4</button>
-          <button id="five">5</button>
-          <button id="six">6</button>
-          <button id="seven">7</button>
-          <button id="eight">8</button>
-          <button id="nine">9</button>
-          <button id="add">+</button>
-          <button id="subtract">-</button>
-          <button id="multiply">x</button>
-          <button id="divide">/</button>
-          <button id="decimal">.</button>
-          <button id="clear">AC</button>
+          <div id="display">{display === "" ? "0": display}</div>
+          <button id="equals" onClick={handleCLick}>=</button>
+          <button id="zero" onClick={handleCLick}>0</button>
+          <button id="one" onClick={handleCLick}>1</button>
+          <button id="two" onClick={handleCLick}>2</button>
+          <button id="three" onClick={handleCLick}>3</button>
+          <button id="four" onClick={handleCLick}>4</button>
+          <button id="five" onClick={handleCLick}>5</button>
+          <button id="six" onClick={handleCLick}>6</button>
+          <button id="seven" onClick={handleCLick}>7</button>
+          <button id="eight" onClick={handleCLick}>8</button>
+          <button id="nine" onClick={handleCLick}>9</button>
+          <button id="add" onClick={handleCLick}>+</button>
+          <button id="subtract" onClick={handleCLick}>-</button>
+          <button id="multiply" onClick={handleCLick}>*</button>
+          <button id="divide" onClick={handleCLick}>/</button>
+          <button id="decimal" onClick={handleCLick}>.</button>
+          <button id="clear" onClick={handleCLick}>AC</button>
         </div>
       </div>
-      <span id="signature">Coded by Zac Horton</span>
+      <span id="signature">
+        Coded by{" "}
+        <a href="https://github.com/ZacHorton" target="_blank">
+          Zac Horton
+        </a>
+      </span>
     </>
   );
 }
--- front-end-development-libraries/projects/javaScript-calculator/src/main.jsx
+++ front-end-development-libraries/projects/javaScript-calculator/src/main.jsx
@@ -1,9 +1,19 @@
+// "Note: React 18 has known incompatibilities with the tests for this project (see issue #45922)"
+// React 18
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+// React 17
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom";
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(<App />,  document.getElementById("root"));
