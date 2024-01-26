8330abce2373fbad53d2edc41e0fd68cffad79fd
Zac Horton <fullstackzac@gmail.com>
2024-01-25 21:56:35 -0500

    Add style and a placeholder bar chart

--- certifications/data-visualization/bar-chart/BarChart.jsx
+++ certifications/data-visualization/bar-chart/BarChart.jsx
@@ -1,11 +1,48 @@
+import { useState, useRef, useEffect } from "react";
+import * as d3 from "d3";
 import "./styles.css";
+import gdpData from "./gdp-data.json";
 
 export function BarChart() {
+  const svgRef = useRef();
+  const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 
+  useEffect(() => {
+    const w = 800;
+    const h = 160;
 
-    return (
-        <>
+    const svg = d3.select(svgRef.current)
+                  .attr("width", w)
+                  .attr("height", h);
 
-        </>
-    );
+    svg.selectAll("rect")
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 30)
+      .attr("y", (d, i) => h - 3 * d)
+      .attr("width", 25)
+      .attr("height", (d, i) => d * 3)
+      .attr("fill", "navy")
+      .attr("class", "bar")
+      .append("title")
+      .text((d) => d);
+
+    svg.selectAll("text")
+      .data(dataset)
+      .enter()
+      .append("text")
+      .text((d) => d)
+      .attr("x", (d, i) => i * 30)
+      .attr("y", (d, i) => h - (d * 3 + 3));
+  }, [dataset]);
+
+  return (
+    <div className="barChartContainer">
+      <div className="container">
+        <div id="title">United States GDP</div>
+        <svg ref={svgRef}></svg>
+      </div>
+    </div>
+  );
 }
