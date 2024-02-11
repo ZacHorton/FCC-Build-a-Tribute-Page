114da2c82bb09086a57a1cdcb6b14d89919208ca
Zac Horton <fullstackzac@gmail.com>
2024-02-10 22:27:52 -0500

    Add tooltip

--- certifications/data-visualization/heat-map/HeatMap.jsx
+++ certifications/data-visualization/heat-map/HeatMap.jsx
@@ -37,12 +37,13 @@ export function HeatMap() {
 
     const xAxis = d3.axisBottom(xScale);
 
-    const yAxis = d3.axisLeft(yScale).tickFormat(function (month) {
+    const formatMonth = function(month) {
       var date = new Date(0);
       date.setUTCMonth(month);
       var format = d3.utcFormat("%B");
       return format(date);
-    });
+    };
+    const yAxis = d3.axisLeft(yScale).tickFormat(formatMonth);
 
     const svg = d3.select(svgRef.current).attr("width", w).attr("height", h);
 
@@ -58,6 +59,12 @@ export function HeatMap() {
       .attr("transform", "translate(" + padding + ", 0)")
       .call(yAxis);
 
+      const tooltip = d3
+      .select(".container")
+      .append("div")
+      .attr("id", "tooltip")
+      .style("opacity", 0);
+
     svg
       .selectAll("rect")
       .data(dataset.monthlyVariance)
@@ -72,7 +79,23 @@ export function HeatMap() {
       .attr("x", (d) => xScale(d.year))
       .attr("y", (d) => yScale(d.month.getMonth()))
       .attr("width", rectW)
-      .attr("height", rectH);
+      .attr("height", rectH)
+      .on("mousemove", function (event, d) {
+        const [x, y] = d3.pointer(event);
+        const mouseX = event.pageX;
+        const mouseY = event.pageY;
+        tooltip
+          .attr("data-year", d.year.getFullYear())
+          .style("opacity", 1)
+          .style("left", mouseX + 10 + "px")
+          .style("top", mouseY + 10 + "px")
+          .html(`${d.year.getFullYear()} - ${formatMonth(d.month.getMonth())}<br />
+          ${(dataset.baseTemperature + d.variance).toFixed(1)}<br />
+          ${d.variance.toFixed(1)}`);
+      })
+      .on("mouseout", function () {
+        tooltip.style("opacity", 0);
+      });
   }, [dataset]);
 
   return (
