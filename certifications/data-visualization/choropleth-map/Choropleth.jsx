3f43ee037fe64b16e73e17e88bfa61ffdeba8923
Zac Horton <fullstackzac@gmail.com>
2024-02-12 19:24:47 -0500

    Remove draw map function and format document

--- certifications/data-visualization/choropleth-map/Choropleth.jsx
+++ certifications/data-visualization/choropleth-map/Choropleth.jsx
@@ -17,43 +17,39 @@ export function Choropleth() {
       .select(".container")
       .append("div")
       .attr("id", "tooltip")
-      .style("opacity", 0)
+      .style("opacity", 0);
 
-    let drawMap = () => {
-      svg
-        .selectAll("path")
-        // Convert county data from topoJSON to d3 supported geoJSON format
-        .data(feature(countyData, countyData.objects.counties).features)
-        .enter()
-        .append("path")
-        .attr("d", d3.geoPath())
-        .attr("class", "county")
-        .attr("data-fips", (d) => d.id)
-        .attr("data-education", (d) => {
-          const county = educationData.find(({ fips }) => fips === d.id);
-          return county.bachelorsOrHigher;
-        })
-        .on("mousemove", function (event, d) {
-          const [x, y] = d3.pointer(event);
-          const mouseX = event.pageX;
-          const mouseY = event.pageY;
-          const base = educationData.find(({ fips }) => fips === d.id)
-          const county = base.area_name;
-          const state = base.state;
-          const percentage = base.bachelorsOrHigher;
-          tooltip
-            .attr("data-education", percentage)
-            .style("opacity", 1)
-            .style("left", mouseX + 10 + "px")
-            .style("top", mouseY + 10 + "px")
-            .html(`${county}, ${state}: ${percentage}%`);
-        })
-        .on("mouseout", function () {
-          tooltip.style("opacity", 0);
-        });
-    };
-
-    drawMap();
+    svg
+      .selectAll("path")
+      // Convert county data from topoJSON to d3 supported geoJSON format
+      .data(feature(countyData, countyData.objects.counties).features)
+      .enter()
+      .append("path")
+      .attr("d", d3.geoPath())
+      .attr("class", "county")
+      .attr("data-fips", (d) => d.id)
+      .attr("data-education", (d) => {
+        const county = educationData.find(({ fips }) => fips === d.id);
+        return county.bachelorsOrHigher;
+      })
+      .on("mousemove", function (event, d) {
+        const [x, y] = d3.pointer(event);
+        const mouseX = event.pageX;
+        const mouseY = event.pageY;
+        const base = educationData.find(({ fips }) => fips === d.id);
+        const county = base.area_name;
+        const state = base.state;
+        const percentage = base.bachelorsOrHigher;
+        tooltip
+          .attr("data-education", percentage)
+          .style("opacity", 1)
+          .style("left", mouseX + 10 + "px")
+          .style("top", mouseY + 10 + "px")
+          .html(`${county}, ${state}: ${percentage}%`);
+      })
+      .on("mouseout", function () {
+        tooltip.style("opacity", 0);
+      });
   }, [svgRef]);
 
   return (
