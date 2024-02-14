c1cd080d0932216534a6f6313f486f85605ea964
Zac Horton <fullstackzac@gmail.com>
2024-02-14 11:51:56 -0500

    Construct root node and treemap layout then compute

--- certifications/data-visualization/treemap-diagram/Treemap.jsx
+++ certifications/data-visualization/treemap-diagram/Treemap.jsx
@@ -11,6 +11,20 @@ export function Treemap() {
     const h = 600;
     let svg = d3.select(svgRef.current).attr("width", w).attr("height", h);
 
+    // Constructs a root node from the specified hierarchical data
+    const root = d3.hierarchy(movieSales);
+
+    // Construct the treemap layout.
+    const treemap = d3.treemap();
+    treemap.size([w, h]);
+    treemap.padding(1);
+
+    // Sum and sort the data.
+    root.sum((d) => d.value);
+    root.sort((a, b) => b.value - a.value);
+
+    // Compute the treemap layout.
+    treemap(root);
   }, [svgRef]);
 
   return (
