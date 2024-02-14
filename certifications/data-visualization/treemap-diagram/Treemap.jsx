3fecfc6379f6f8a9c3e7af817ab544c76eb9d488
Zac Horton <fullstackzac@gmail.com>
2024-02-14 12:10:47 -0500

    Add rectangles with conditional fill and set various data attributes

--- certifications/data-visualization/treemap-diagram/Treemap.jsx
+++ certifications/data-visualization/treemap-diagram/Treemap.jsx
@@ -11,7 +11,7 @@ export function Treemap() {
     const h = 600;
     let svg = d3.select(svgRef.current).attr("width", w).attr("height", h);
 
-    // Constructs a root node from the specified hierarchical data
+    // Constructs a root node from the specified hierarchical data.
     const root = d3.hierarchy(movieSales);
 
     // Construct the treemap layout.
@@ -25,6 +25,53 @@ export function Treemap() {
 
     // Compute the treemap layout.
     treemap(root);
+
+    const leaf = svg
+      .selectAll("g")
+      // Returns the array of leaf nodes in traversal order.
+      // A leaf is a node with no children.
+      .data(root.leaves())
+      .enter()
+      .append("g")
+      .attr("transform", (movie) => {
+        return "translate(" + movie.x0 + ", " + movie.y0 + ")";
+      });
+
+    leaf
+      .append("rect")
+      .attr("width", (movie) => {
+        return movie.x1 - movie.x0;
+      })
+      .attr("height", (movie) => {
+        return movie.y1 - movie.y0;
+      })
+      .attr("class", "tile")
+      .attr("fill", (movie) => {
+        if (movie.data.category === "Action") {
+          return "#f94144";
+        } else if (movie.data.category === "Adventure") {
+          return "#f3722c";
+        } else if (movie.data.category === "Family") {
+          return "#f8961e";
+        } else if (movie.data.category === "Biography") {
+          return "#f9c74f";
+        } else if (movie.data.category === "Animation") {
+          return "#90be6d";
+        } else if (movie.data.category === "Comedy") {
+          return "#43aa8b";
+        } else if (movie.data.category === "Drama") {
+          return "#577590";
+        }
+      })
+      .attr("data-name", (movie) => {
+        return movie.data.name;
+      })
+      .attr("data-category", (movie) => {
+        return movie.data.category;
+      })
+      .attr("data-value", (movie) => {
+        return movie.data.value;
+      });
   }, [svgRef]);
 
   return (
